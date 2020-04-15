672b5d445f49ebc7afed7f2efd08c2e7d11bf100
Rameshrs777 <44388916+Rameshrs777@users.noreply.github.com>
2020-04-15 22:45:25 +0530

    Delete vars.tf

--- vars.tf
+++
@@ -1,9 +0,0 @@
-variable "access_key" {
-    description = "Your AWS Access Key"
-    default = "AKIAVWCRJNBRPTZTS75J"
-}
-
-variable "secret_key" {
-    description = "Your AWS Secret Key"
-    default = "8RFx4UU/7gtTeNqQCerRKhiuNkRbpZQ0A9TlwZ2r"
-}
